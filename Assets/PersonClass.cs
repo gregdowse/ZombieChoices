99ffa4904fe55b4826e2501032f8f9e33c0da9fa
Dowse <gregory.dowse@ldngol-nmxd2bu.emea.corp.ipgnetwork.com>
2017-09-18 17:10:18 +0100

    Initial Set Up

---
+++ Assets/PersonClass.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonClass {
+
+	public string FirstName;
+	public string LastName;
+	public int Age;
+	public int Health;
+
+}
